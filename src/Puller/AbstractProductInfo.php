01dd0b3f436f99ab69e193c915b127f9c0f4f921
Evaldo Barbosa <tryadesoftware@gmail.com>
2013-05-16 14:29:28 -0300

    Update AbstractProductInfo.php

--- src/Puller/AbstractProductInfo.php
+++ src/Puller/AbstractProductInfo.php
@@ -1,10 +1,31 @@
 <?php
+/*******************************************************************************
+* Copyright 2012 Evaldo Barbosa
+*
+* This file is part of Product Puller.
+*
+* Product Puller is free software; you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation; either version 2 of the License, or
+* (at your option) any later version.
+*
+* Product Puller is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with Product Puller; if not, write to the Free Software
+* Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+*
+*******************************************************************************/
+
 namespace Puller;
 
 use Infra\Utils\RestRequest;
 
 abstract class AbstractProductInfo {
-  private $cacheFileName;
+	private $cacheFileName;
 	private $url;
 	protected $id;
 	protected $name;
