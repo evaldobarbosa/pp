d9135079e3452fe2367de523ce95436a33939e1c
Evaldo Barbosa <evaldobarbosa@gmail.com>
2013-06-18 17:39:20 -0300

    Update ProductInfoTest.php
    Magazine Luiza

--- tests/src/Puller/ProductInfoTest.php
+++ tests/src/Puller/ProductInfoTest.php
@@ -25,6 +25,7 @@ use Puller\Target\SaraivaProductInfo;
 use Puller\Target\NetShoesProductInfo;
 use Puller\Target\PontoFrioProductInfo;
 use Puller\Target\RicardoEletroProductInfo;
+use Puller\Target\MagazineLuizaProductInfo;
 /**
  * @group Info
  * @author evaldo
@@ -95,6 +96,19 @@ class ProductInfoTest extends PHPUnit_Framework_TestCase {
 		unset( $request );
 		$request = $this->createRequest("ricardoeletro");
 		$this->assertTrue( file_exists($filename) );
+		
+		/** Magazine Luiza **/
+		$request = $this->createRequest("magazineluiza");
+		$hash = md5($request->productURL);
+		$filename = "/tmp/MagazineLuizaProductInfo-{$hash}.html";
+		
+		unlink( $filename );
+		
+		$this->assertFalse( file_exists($filename) );
+		
+		unset( $request );
+		$request = $this->createRequest("magazineluiza");
+		$this->assertTrue( file_exists($filename) );
 	}
 	
 	/**
@@ -118,6 +132,9 @@ class ProductInfoTest extends PHPUnit_Framework_TestCase {
 		
 		$request = $this->createRequest("ricardoeletro");
 		$this->assertEquals('326629', $request->productId);
+		
+		$request = $this->createRequest("magazineluiza");
+		$this->assertEquals('0867153', $request->productId);
 	}
 	
 	/**
@@ -147,6 +164,10 @@ class ProductInfoTest extends PHPUnit_Framework_TestCase {
 		$request = $this->createRequest("ricardoeletro");
 		$productName = 'Celular Desbloqueado LG Optimus L5 II Dual E455 Preto - Dual Chip, Android 4.1, Câmera 5 MP, Wi-Fi, MP3, GPS, Bluetooth e Rádio FM';
 		$this->assertEquals( $productName, $request->productName );
+		
+		$request = $this->createRequest("magazineluiza");
+		$productName = 'Smartphone 3G Dual Chip LG Optimus L5 II - Android 4.1 Câmera 5MP Tela 4" Wi-Fi';
+		$this->assertEquals( $productName, $request->productName );
 	}
 	
 	/**
@@ -169,7 +190,10 @@ class ProductInfoTest extends PHPUnit_Framework_TestCase {
 		$this->assertEquals('999,00',$request->productPrice);
 		
 		$request = $this->createRequest("ricardoeletro");
-		$this->assertEquals('839,00',$request->productPrice);
+		$this->assertEquals('749,00',$request->productPrice);
+		
+		$request = $this->createRequest("magazineluiza");
+		$this->assertEquals('779,00',$request->productPrice);
 	}
 	
 	/**
@@ -184,7 +208,7 @@ class ProductInfoTest extends PHPUnit_Framework_TestCase {
 	/**
 	 * @depends testIfRequestCanCreateCacheFile
 	 */
-	function testIfTableDescriptionAreCorrect() {
+	function testIfTableDescriptionIsCorrect() {
 		$request = $this->createRequest("submarino");
 		
 		$info = $request->productTable;
@@ -238,6 +262,17 @@ class ProductInfoTest extends PHPUnit_Framework_TestCase {
 		$this->assertTrue( $info->offsetExists('Flash Integrado') );
 		
 		$this->assertEquals( 'Sim', $info->offsetGet('EDGE') );
+		
+		/** MAGAZINE LUIZA **/
+		$request = $this->createRequest("magazineluiza");
+		
+		$info = $request->productTable;
+		
+		$this->assertTrue( $info->offsetExists('Marca') );
+		$this->assertTrue( $info->offsetExists('Cor') );
+		$this->assertTrue( $info->offsetExists('3G') );
+		
+		$this->assertEquals( 'DualBand', $info->offsetGet('3G') );
 	}
 	
 	/**
@@ -255,38 +290,45 @@ class ProductInfoTest extends PHPUnit_Framework_TestCase {
 		
 		$request = $this->createRequest("americanas");
 		$this->assertEquals(
-				'http://img.americanas.com.br/produtos/01/00/item/111970/0/111970051G1.jpg',
-				$request->productPicture
+			'http://img.americanas.com.br/produtos/01/00/item/111970/0/111970051G1.jpg',
+			$request->productPicture
 		);
 		
 		unset($request);
 		
 		$request = $this->createRequest("saraiva");
-		$this->assertEquals(
-				'http://images.livrariasaraiva.com.br/imagem/imagem.dll?pro_id=4710182&L=500&A=-1&PIM_Id=',
-				$request->productPicture
+		$arImg = array(
+			'http://images.livrariasaraiva.com.br/imagemnet/imagem.aspx?pro_id=4710182&L=500&A=-1&PIM_Id='=>1,
+			'http://images.livrariasaraiva.com.br/imagem/imagem.dll?pro_id=4710182&L=500&A=-1&PIM_Id='=>1
 		);
+		$this->arrayHasKey($request->productPicture);
 		
 		unset($request);
 		
 		$request = $this->createRequest("netshoes");
 		$this->assertEquals(
-				'http://nsh2.br.netshoes.net/Produtos/06/094-0460-006/094-0460-006_janela.jpg',
-				$request->productPicture
+			'http://nsh2.br.netshoes.net/Produtos/06/094-0460-006/094-0460-006_janela.jpg',
+			$request->productPicture
 		);
 		
 		unset($request);
 		
 		$request = $this->createRequest("pontofrio");
 		$this->assertEquals(
-				'http://www.pontofrio-imagens.com.br/Control/ArquivoExibir.aspx?IdArquivo=6549950',
-				$request->productPicture
+			'http://www.pontofrio-imagens.com.br/Control/ArquivoExibir.aspx?IdArquivo=6549950',
+			$request->productPicture
 		);
 		
 		$request = $this->createRequest("ricardoeletro");
 		$this->assertEquals(
-				'http://images.maquinadevendas.com.br/370x370/produto/326629_2004826_20130416164443.jpg',
-				$request->productPicture
+			'http://images.maquinadevendas.com.br/370x370/produto/326629_2004826_20130416164443.jpg',
+			$request->productPicture
+		);
+		
+		$request = $this->createRequest("magazineluiza");
+		$this->assertEquals(
+			'http://i.mlcdn.com.br/410x308/smartphone-3g-dual-chip-lg-optimus-l5-iiandroid-4.1-camera-5mp-tela-4-wi-fi-086715300.jpg',
+			$request->productPicture
 		);
 	}
 	
@@ -304,6 +346,8 @@ class ProductInfoTest extends PHPUnit_Framework_TestCase {
 				return new PontoFrioProductInfo( 'TelefoneseCelulares/Smartphones/Celular-Desbloqueado-Motorola-RAZR-i-Preto-com-Processador-Intel-de-2-GHz-Tela-de-4-3’’-Android-4-0-Camera-8MP-Wi-Fi-3G-NFC-GPS-e-Bluetooth-1748861.html' );
 			case "ricardoeletro":
 				return new RicardoEletroProductInfo( 'Celular-Desbloqueado-LG-Optimus-L5-II-Dual-E455-Preto-Dual-Chip-Android-41-Camera-5-MP-Wi-Fi-MP3-GPS-Bluetooth-e-Radio-FM/44-491-497-296172' );
+			case "magazineluiza":
+				return new MagazineLuizaProductInfo( 'smartphone-3g-dual-chip-lg-optimus-l5-ii-android-4.1-camera-5mp-tela-4-wi-fi/p/0867153/te/tece/' );
 		}
 	}
 }
